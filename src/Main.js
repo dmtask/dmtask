f2b88f5b2b7c8ced540562a977f4a8b573334f0a
Daniel Mertgen <dm@eoceo.de>
2020-04-14 17:41:38 +0200

    aktueller Stand

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,37 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import Bild from './bild3.jpg';
 
-const projectNames = ['docker-ruby-node','multi-release-manager','delete-old-logs','ruler'];
-const projectReleases = [];
+require('dotenv').config();
+
+const projects = [{
+    name: 'docker-ruby-node',
+    description: 'A Dockerfile for ruby 2.6.5 and nodejs 12',
+    release: false
+},
+{
+    name: 'multi-release-manager',
+    description: 'Multi Versionierungs Tool für Ruby gems',
+    release: true
+},
+{
+    name: 'delete-old-logs',
+    description: '',
+    release: true
+},
+{
+    name: 'ruler',
+    description: 'Ein HTML Lineal als App für Mac',
+    release: true
+}];
 
 function Main() {
-    getReleaseFromGithub((releases) => {
-        console.log(releases);
-    });
+    console.log(process.env.REACT_APP_GITHUBTOKEN);
 
     return (
         <main role="main">
         <div className="row">
             <div className="col-12">
-                <img src={Bild} alt="" className="img-fluid" />
+                <img src={Bild} alt="Headerbild" className="img-fluid" />
             </div>
         </div>
         <div className="container">
@@ -24,7 +42,7 @@ function Main() {
         </div>
             <div className="row mt-2 mb-4" id="projects">
                 <div className="col-12">
-                    {getProjectName()}
+                    {getProjects()}
                 </div>
             </div>
         </div>
@@ -32,55 +50,46 @@ function Main() {
     );
 }
 
-function getReleaseFromGithub(callback) {
-    projectNames.forEach((projectName, index) => {
-        fetch('https://api.github.com/repos/dmtask/' + projectName + '/releases/latest', {
+function getReleaseFromGithub(project, callback) {
+    fetch('https://api.github.com/repos/dmtask/' + project.name + '/releases/latest', {
             headers: {
-                authorization: 'token 19b3219402c21e5b3ee75693a279e40e93e58316'
+                authorization: 'token ' + process.env.REACT_APP_GITHUBTOKEN
             }
         }).then((r) => {
             if (r.status === 200) {
                 r.json().then((j) => {
-                    projectReleases.push(j);
+                    callback(j);
                 });
-
-                if (index === projectNames.length - 1) {
-                    callback(projectReleases);
-                }
             } else {
                 //console.error(r.url + ' -> Release ' + r.statusText + ' (' + r.status + ')');
+                callback('');
             }
         }).catch((error) => {
             console.error(error);
+            callback('');
         });
-    });
 }
 
-function getProjectDescriptionByName(projectName, callback) {
-    fetch('https://api.github.com/repos/dmtask/' + projectName, {
-        headers: {
-            authorization: 'token 19b3219402c21e5b3ee75693a279e40e93e58316'
-        }
-    }).then((r) => {
-        r.json().then((j) => {
-            console.log(j);
-            console.log(j.description);
-
-            callback(j.description);
-        });
-    });
-}
-
-function getProjectName() {
+function getProjects() {
     let elements = [];
 
-    projectNames.forEach((projectName, index) => {
+    projects.forEach((project, index) => {
+        let githubLink = "https://github.com/dmtask/" + project.name;
+        let downloadLink = '';
+        if (project.release) {
+            getReleaseFromGithub(project, (release) => {
+                console.log(release);
+                downloadLink = <a href='#' className="btn btn-primary" title="Letztes Release runterladen">Download</a>;
+            });
+        }
+
         elements.push(
             <div key={index} className="card mr-4 mb-4">
                 <div className="card-body">
-                    <h5 className="card-title">{projectName}</h5>
-                    <p className="card-text">{getProjectDescriptionByName(projectName, (description) => { })}</p>
-                    <a href="#" className="btn btn-primary"></a>
+                    <h5 className="card-title">{project.name}</h5>
+                    <p className="card-text">{project.description}</p>
+                    {downloadLink}
+                    <a href={githubLink} className="btn">Github öffnen</a>
                 </div>
             </div>
         );
