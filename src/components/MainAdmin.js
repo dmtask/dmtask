83be6a2deaf7c290b18208aa7efa4d1899a42141
Daniel Mertgen <dm@eoceo.de>
2020-08-03 12:04:25 +0200

    Admin Inhalt

--- src/components/MainAdmin.js
+++ src/components/MainAdmin.js
@@ -8,21 +8,12 @@ function MainAdmin() {
         <main role="main" className="container-fluid">
             <div className="row flex-xl-nowrap mt-4">
                 <div className="col-12">
-                    <form>
-                        <input type="text" placeholder="Tätigkeit" className="form-control" id="description" />
-                    </form>
+                    Hallo Admin
                 </div>
             </div>
             <div className="row mt-4">
                 <div className="col-12">
-                    <table className="table">
-                        <thead>
-                            
-                        </thead>
-                        <tbody>
-                            
-                        </tbody>
-                    </table>
+                    
                 </div>
             </div>
         </main>
