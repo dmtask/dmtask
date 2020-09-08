fccb96fef290b1c6048d5117f7e6512c5cf27d8d
Daniel Mertgen <dm@eoceo.de>
2020-09-08 12:59:28 +0200

    diesen drecks login -> admin redirect gefixt

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Redirect} from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import firebase from "firebase/app";
@@ -18,13 +19,13 @@ class Login extends React.Component {
     }
 
     handleSubmit(event) {
+        event.preventDefault();
+
         let password = this.state.value;
 
         firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(function() {
             firebase.auth().signInWithEmailAndPassword(process.env.REACT_APP_LOGINEMAIL, password).then(function() {
-                if (firebase.auth().currentUser) {
-                    window.location = window.origin + '/#/admin';
-                }
+                window.location.reload();
             }).catch(function(error) {
                 document.querySelector('.errorsLogin > div').innerHTML = error.code + ': ' + error.message;
                 document.getElementsByClassName('errorsLogin')[0].style.display = 'block';
@@ -33,25 +34,31 @@ class Login extends React.Component {
     }
 
     render() {
-        return (
-            <React.Fragment>
-                <Header optionalTitle='ADMIN' isAdmin={true} />
-                <main role="main" className="container-fluid">
-                    <div className="row flex-xl-nowrap mt-4 errorsLogin">
-                        <div className="alert alert-danger" role="alert"></div>
-                    </div>
-                    <div className="row flex-xl-nowrap mt-4 mb-4">
-                        <div className="col-12 text-center">
-                            <form action="/#/login" onSubmit={this.handleSubmit}>
-                                <input type="password" value={this.state.value} onChange={this.handleChange} placeholder="Passwort" className="form-control" />
-                                <input type="submit" value="Login" className="btn btn-primary" />
-                            </form>
+        const user = sessionStorage.getItem('firebase:authUser:' + process.env.REACT_APP_FIREBASE_apiKey + ':[DEFAULT]');
+
+        if (user !== null) {
+            return (<Redirect to="/admin" />);
+        } else {
+            return (
+                <React.Fragment>
+                    <Header optionalTitle='ADMIN' isAdmin={true} />
+                    <main role="main" className="container-fluid">
+                        <div className="row flex-xl-nowrap mt-4 errorsLogin">
+                            <div className="alert alert-danger" role="alert"></div>
                         </div>
-                    </div>
-                </main>
-                <Footer />
-            </React.Fragment>
-        );
+                        <div className="row flex-xl-nowrap mt-4 mb-4">
+                            <div className="col-12 text-center">
+                                <form action="/#/login" onSubmit={this.handleSubmit}>
+                                    <input type="password" value={this.state.value} onChange={this.handleChange} placeholder="Passwort" className="form-control" />
+                                    <input type="submit" value="Login" className="btn btn-primary" />
+                                </form>
+                            </div>
+                        </div>
+                    </main>
+                    <Footer />
+                </React.Fragment>
+            );
+        }
     }
 }
 
